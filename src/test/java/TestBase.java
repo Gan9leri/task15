832ca20642183d20d3c6564a0bdf54dffed1b3b0
AOvsyannikov <aovsyannikov@it-one.ru>
2024-06-12 17:14:57 +0300

    added TestBase.java

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,10 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+    }
+}
